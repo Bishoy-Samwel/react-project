88faa5aac1fa0e725203ee19e3bf4f3e6aa967df
Bishoy <bishoy.samwuel@gmail.com>
2021-09-15 16:33:07 +0200

    Import TodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
+import TodoList from './todos/TodoList';
 import './App.css';
 
 const App = () => (
   <div className="App">
-    <h1>Hello, world!</h1>
+    <h1>TodoList!</h1>
+    <TodoList />
   </div>
 );
 
