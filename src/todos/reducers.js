e2b3b08652f405944f2164a16bc57292638f4ea1
Bishoy <bishoy.samwuel@gmail.com>
2021-09-16 17:11:46 +0200

    Fix a typo

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,6 +1,6 @@
 import { CREATE_TODO, REMOVE_TODO } from './actions'
 export const todos = (state = [], action) => {
-  const { type, paylaod } = action;
+  const { type, payload } = action;
 
   switch (type) {
     case CREATE_TODO: {
