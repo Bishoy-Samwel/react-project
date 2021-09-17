98d05b627d935a49c1e993f68d9a5a2c6f085599
Bishoy <bishoy.samwuel@gmail.com>
2021-09-17 11:45:55 +0200

    Add remove item functionality Using useDispatch

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './TodoListItem.css';
+import { useDispatch } from 'react-redux';
+import { removeTodo } from './actions'
 
-const TodoListItem = ({ todo }) => (
-    <div className="todo-item-container">
-        <h3>{todo.text}</h3>
-        <div className="buttons-container">
-            <button className="completed-button">Mark As Completed</button>
-            <button className="remove-button">Remove</button>
+const TodoListItem = ({ todo }) => {
+    const dispatch = useDispatch();
+    return (
+        <div className="todo-item-container">
+            <h3>{todo.text}</h3>
+            <div className="buttons-container">
+                <button className="completed-button">Mark As Completed</button>
+                <button
+                    onClick={
+                        () => {
+                            dispatch(removeTodo(todo.text));
+                            }}
+                    className="remove-button">Remove</button>
+            </div>
         </div>
-    </div>
-);
+    );
+}
+
+
 
 export default TodoListItem;
