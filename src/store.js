63910b9e5258e73d7a93d7bdf7e9e329d74a1159
Bishoy <bishoy.samwuel@gmail.com>
2021-09-18 14:37:38 +0200

    Install Thunk imporrt applyMiddleware,  composeWithDevTools

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-import {createStore, combineReducers} from 'redux';
-import {todos} from './todos/reducers'
+import { createStore, applyMiddleware , combineReducers } from 'redux';
+import { todos } from './todos/reducers'
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   todos
 });
 
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () => createStore(
+  rootReducer, 
+  composeWithDevTools(applyMiddleware(thunk)));
