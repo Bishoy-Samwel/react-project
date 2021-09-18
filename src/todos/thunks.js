b6e6e5921d5b7ab464fc898080c455b166b48f23
Bishoy <bishoy.samwuel@gmail.com>
2021-09-18 16:19:34 +0200

    Create a thunk to extract the data, Use dispatch

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,18 @@
-export const displayAlert = () => ()=> {
-  alert('Hello!');
+import { loadTodosSucess, loadTodosFailue, loadTodosInProgress } from './actions';
+
+export const loadTodos = () => async (dispatch, getState) => {
+  try {
+    dispatch(loadTodosInProgress());
+    const response = await fetch('http://localhost:8080/todos');
+    const todos = await response.json();
+
+    dispatch(loadTodosSucess(todos));
+  } catch (e) {
+    dispatch(loadTodosFailue())
+    dispatch(displayAlert(e))
+  }
+}
+
+export const displayAlert = (text) => () => {
+  alert(`${text}`);
 }
