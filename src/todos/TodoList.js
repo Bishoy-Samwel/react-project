886a5cd4ef7c4c41d87297436b96b0342f7a4250
Bishoy <bishoy.samwuel@gmail.com>
2021-09-20 13:41:45 +0200

    Loading todod: Using dispatch loadTodos Extract isLoading Show the list or the loading message

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 import { useSelector, shallowEqual } from 'react-redux';
+import { loadTodos, displayAlert } from './thunks'
+import { useDispatch } from 'react-redux';
+
 
 const TodoList = () => {
+    const dispatch = useDispatch();
+    useEffect(
+        () => { dispatch(loadTodos()) }, []);
+    const isLoading = useSelector((state) => state.isLoading, shallowEqual);
     const todos = useSelector((state) => state.todos, shallowEqual);
-    return (
-        <div className="list-wrapper">
-            <NewTodoForm />
-            {console.log(todos)}
-            {todos.map(todo => <TodoListItem todo={todo} />)}
-        </div>
-    );
+
+    const loadingMessage = <div> Loading todos...</div>;
+    const content = (todos) =>
+    (<div className="list-wrapper">
+        <NewTodoForm />
+        {todos.map(todo => <TodoListItem todo={todo} />)}
+    </div>);
+    if (Array.isArray(isLoading)) {
+        return content(isLoading)
+    }
+    else {
+        return loadingMessage;
+    }
 }
 
 export default TodoList;
