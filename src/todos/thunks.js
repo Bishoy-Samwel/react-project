efe0c75852217283626fdd4f621120a7eed3166a
Bishoy <bishoy.samwuel@gmail.com>
2021-09-20 18:37:09 +0200

    Create a new thunk to add new todo to server: - and Calling dispatch createTodo after server step

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,16 +1,38 @@
-import { loadTodosSuccess, loadTodosFailure, loadTodosInProgress } from './actions';
+import { loadTodosSuccess, loadTodosFailure, loadTodosInProgress, createTodo } from './actions';
 
-export const loadTodos = () => async (dispatch, getState) => {
-  try {
-    dispatch(loadTodosInProgress());
-    const response = await fetch('http://localhost:8080/todos');
-    const todos = await response.json();
-    dispatch(loadTodosSuccess(todos));
-  } catch (e) {
-    dispatch(loadTodosFailure());
-    dispatch(displayAlert(e));
+export const loadTodos = () => (
+  async (dispatch) => {
+    try {
+      dispatch(loadTodosInProgress());
+      const response = await fetch('http://localhost:8080/todos');
+      const todos = await response.json();
+
+      dispatch(loadTodosSuccess(todos));
+    } catch (e) {
+      dispatch(loadTodosFailure());
+      dispatch(displayAlert(e));
+    }
   }
-}
+)
+
+export const addTodoRequest = text => (
+  async (dispatch) => {
+    try {
+    const body = JSON.stringify({ text });
+    const response = await fetch('http://localhost:8080/todos', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'post',
+      body,
+    });
+    const todo = await response.json();
+    dispatch(createTodo(todo));
+    } catch (error) {
+      dispatch(displayAlert(error))
+    }
+  }
+)
 
 export const displayAlert = (text) => () => {
   alert(`${text}`);
