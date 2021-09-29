74a36050068611f82a4d1e17e3adad3ae55b6fd6
Bishoy <bishoy.samwuel@gmail.com>
2021-09-29 18:11:57 +0200

    Use the selectors to sperate completed from incompleted tasks

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -3,25 +3,32 @@ import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 import { useSelector, shallowEqual } from 'react-redux';
-import { loadTodos, displayAlert } from './thunks'
+import { loadTodos } from './thunks'
 import { useDispatch } from 'react-redux';
-
+import { selectCompletedTodos, selectInCompletedTodos, loading } from './selectors';
 
 const TodoList = () => {
     const dispatch = useDispatch();
-    useEffect(
-        () => { dispatch(loadTodos()) }, []);
-    const isLoading = useSelector((state) => state.isLoading, shallowEqual);
-    const todos = useSelector((state) => state.todos, shallowEqual);
+    const isLoading = useSelector(loading);
+    const completedTodos = useSelector(selectCompletedTodos);
+    const uncompletedTodos = useSelector(selectInCompletedTodos);
+
+    useEffect( () => { dispatch(loadTodos()) }, []  );
 
     const loadingMessage = <div> Loading todos...</div>;
     const content = (todos) =>
-        (<div className="list-wrapper">
-            <NewTodoForm />
-            {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
-        </div>);
+    (<div className="list-wrapper">
+        <NewTodoForm />
+        {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
+    </div>);
     if ((!isLoading)) {
-        return content(todos)
+
+        return <div>
+            Uncompleted Todos
+            {content(uncompletedTodos)}
+            Completed todos
+            {content(completedTodos)}
+        </div>
     }
     else {
         return loadingMessage;
