62cc2bd3f2f99596634ae33dc042e9bd302f0dd3
Bishoy <bishoy.samwuel@gmail.com>
2021-10-01 14:04:45 +0200

    Transform css  to style component of TodoListItem

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,28 +1,61 @@
 import React from 'react';
-import './TodoListItem.css';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import { markCompletedRequest, removeTodoRequest } from './thunks';
+
+const TodoItemContainer = styled.div`    background: #fff;
+    border-radius: 8px;
+    margin-top: 8px;
+    padding: 16px;
+    position: relative;
+    box-shadow: 0 4px 8px grey;
+    `
+const ButtonsContainer = styled.div`
+position: absolute;
+right: 12px;
+bottom: 12px;`;
+
+const Complete = styled.button`
+font-size: 16px;
+padding: 8px;
+border: none;
+border-radius: 8px;
+outline: none;
+cursor: pointer;
+display: inline-block;
+background-color: #22ee22;`;
+
+const Remove = styled.button`
+font-size: 16px;
+padding: 8px;
+border: none;
+border-radius: 8px;
+outline: none;
+cursor: pointer;
+display: inline-block;
+background-color: #ee2222;
+margin-left: 8px;`;
 
 const TodoListItem = ({ todo }) => {
     const dispatch = useDispatch();
     return (
-        <div className="todo-item-container">
+        <TodoItemContainer>
             <h3>{todo.text}</h3>
-            <div className="buttons-container">
-            {todo.isCompleted? null:
-                <button onClick={
-                    () => {
-                        dispatch(markCompletedRequest(todo.id));
-                    }}
-                    className="completed-button">Mark As Completed</button>}
-                <button
+            <ButtonsContainer>
+                {todo.isCompleted ? null :
+                    <Complete onClick={
+                        () => {
+                            dispatch(markCompletedRequest(todo.id));
+                        }}
+                        >Mark As Completed</Complete>}
+                <Remove
                     onClick={
                         () => {
                             dispatch(removeTodoRequest(todo.id));
                         }}
-                    className="remove-button">Remove</button>
-            </div>
-        </div>
+                    >Remove</Remove>
+            </ButtonsContainer>
+        </TodoItemContainer>
     );
 }
 
